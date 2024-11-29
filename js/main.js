16f6799a505ca94d141c049478baa1833ec84d0b
jiseolhee <guribellta@gmail.com>
2024-11-29 17:20:53 +0900

    version2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function (){
+$(function () {
   // Splitting.js로 텍스트를 애니메이션 효과 주기
   Splitting();
 
@@ -8,13 +8,30 @@ $(function (){
     let posY = e.pageY;
 
     $('.p1').css({
-      top: -79 - (posY / 40),
-      right: -156 - (posX / 70),
+      top: -79 - (posY / 30),
+      right: -156 - (posX / 10),
     });
     $('.p2').css({
-      top: -79 + (posY / 30),
-      right: -156 + (posX / 40),
+      top: -79 + (posY / 20),
+      right: -156 + (posX / 20),
     });
+  });
 
+  $('#con2 .left li').hover(function () {
+    let i = $(this).index();
+    $('#con2 .img_box img').removeClass('on').eq(i).addClass('on');
+  }, function () {
+    $('#con2 .img_box img').removeClass('on')
+  })
+
+  $('#con4').on('mousemove', function (e) {
+    let posX = e.pageX;
+    let posY = e.pageY;
+
+    $('.p3').css({
+      bottom: -300 - (posY / 100),
+      right: -120 - (posX / 10),
+    });
+    
   });
 });
